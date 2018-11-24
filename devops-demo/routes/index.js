07c27ec0a1e48d2c4b720633d959ae452cd4dd3f
Marcel Juhnke <marrat@marrat.de>
2018-11-24 13:10:53 +0100

    add build gitsha to output

--- devops-demo/routes/index.js
+++ devops-demo/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const info = require('../info.json')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DevOps Hof' });
+  res.render('index', { title: 'DevOps Hof', gitsha: info.gitsha });
 });
 
 module.exports = router;
