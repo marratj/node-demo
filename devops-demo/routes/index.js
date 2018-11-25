69734f39370e75caa9551b0526857d8782ef2e6b
Marcel Juhnke <marrat@marrat.de>
2018-11-25 22:08:37 +0100

    Beginning

--- devops-demo/routes/index.js
+++ devops-demo/routes/index.js
@@ -5,7 +5,7 @@ const info = require('../info.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DevOps Hof Update', gitsha: info.gitsha });
+  res.render('index', { title: 'DevOps Hof', gitsha: info.gitsha });
 });
 
 module.exports = router;
